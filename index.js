7e925636ef312c45d20229f22c8e042ca3431538
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2023-01-09 22:47:08 -0300

    Criando novo projeto com Express Generator

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const consign = require('consign');
-const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
+const express = require("express");
+const consign = require("consign");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
 
 let app = express();
 
@@ -9,10 +9,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(expressValidator());
 
-consign().include('routes').include('utils').into(app);
+consign().include("routes").include("utils").into(app);
 
-app.listen(3000, '127.0.0.1', ()=>{
-
-    console.log('servidor rodando!');
-
+app.listen(4000, "127.0.0.1", () => {
+  console.log("servidor rodando!");
 });
